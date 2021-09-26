d6df36e2e119936756381a2151d36364f60370d9
Amritpal-kaur-17 <amritpal.kaur1707@gmail.com>
2021-09-26 20:02:22 +0200

    v3

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Bookings from "./components/Bookings";
+import Heading from "./components/Heading";
+import TouristInfoCards from "./components/TouristInfoCards";
+import "./App.css";
+import Footer from "./components/Footer";
+import Restaurant from "./components/Restaurant";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Heading />
+      <TouristInfoCards />
+      <Bookings />
+      <Restaurant />
+      <Footer
+        infoArray={[
+          "123 Fake Street, London, E1 4UD",
+          "hello@fakehotel.com",
+          "0123 456789"
+        ]}
+      />
+    </div>
+  );
+};
+
+export default App;
